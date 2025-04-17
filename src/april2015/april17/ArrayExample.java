4a634b365ebf0442bda53c6fe337d9497da0b6f0
Sandeep.pandey <sandeep.pandey15@gmail.com>
2025-04-17 18:58:59 +0530

    Added code

---
+++ src/april2015/april17/ArrayExample.java
@@ -0,0 +1,12 @@
+package april2015.april17;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] a = new int[6];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=i;
+        }
+        System.out.println(a[2]);
+
+    }
+}
