78fe81a54e44fbd299f6959ebfc58dc607256ad6
Sandeep.pandey <sandeep.pandey15@gmail.com>
2025-04-21 15:37:31 +0530

    Added code

--- src/april2015/april21/Pattern.java
+++ src/april2015/april21/Pattern.java
@@ -7,31 +7,24 @@ public class Pattern {
         for (int i = 0; i < 5; i++) {
             if (i == 0) {
                 for (int j = 0; j < 5; j++) {
-                    System.out.print(" * ");
+                    System.out.print("*");
                 }
+                System.out.println();
             }
             if (i > 0 && i < 4) {
-                for (int j = 0; j < 5; j++) {
-                    if (j == 0) {
-                        System.out.print(" * ");
-                    }
-                    if (j > 0 && j < 4) {
-                        System.out.print(" ");
-                    }
-                    if (j == 4) {
-                        System.out.print(" * ");
-                    }
+                System.out.print("*");
+                for (int j = 1; j <= 4; j++) {
+                    System.out.print(" ");
                 }
+                System.out.print("*");
             }
             if (i == 4) {
+                System.out.println();
                 for (int m = 0; m < 5; m++) {
-                    System.out.print(" * ");
+                    System.out.print("*");
                 }
+                System.out.println();
             }
-            System.out.println();
-
         }
-
-
     }
 }
