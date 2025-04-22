5e5f1954320ca84d0718b73022a40234e1302755
Sandeep.pandey <sandeep.pandey15@gmail.com>
2025-04-22 18:52:16 +0530

    Added code

---
+++ src/april2015/april22/JavaException.java
@@ -0,0 +1,34 @@
+package april2015.april22;
+
+public class JavaException {
+    public static void main(String[] args) {
+       try {
+           show();
+       }catch (MyExp2 exp2){
+           exp2.printStackTrace();
+       }
+    }
+    public static void show(){
+        try {
+            int x= 1/0;
+        }catch (RuntimeException exp){
+            throw new MyExp("myexp2");
+        }
+    }
+}
+
+class MyExp extends RuntimeException{
+    String message;
+
+    public MyExp(String message) {
+        super(message);
+    }
+}
+
+class MyExp2 extends RuntimeException{
+    String message;
+
+    public MyExp2(String message) {
+        super(message);
+    }
+}
