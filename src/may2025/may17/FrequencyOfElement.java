8066da2c22dcd5cd46c2cd504637222700f7a87b
Sandeep.pandey <sandeep.pandey15@gmail.com>
2025-05-17 20:22:21 +0530

    Added code

--- src/may2025/may17/FrequencyOfElement.java
+++ src/may2025/may17/FrequencyOfElement.java
@@ -8,13 +8,17 @@ public class FrequencyOfElement {
     public static void main(String[] args) {
         int []a={2,2,4,5,3,2,1,8,9,7,8,5,7,4,2,1,0,1,2};
         Map<Integer,Integer> integerIntegerMap = new HashMap<>();
-        Arrays.stream(a).forEach(i -> {
-            if (integerIntegerMap.get(i) == null) {
-                integerIntegerMap.put(i, 1);
-            } else {
-                integerIntegerMap.compute(i, (k, val) -> val + 1);
-            }
-        });
+        Arrays.stream(a).forEach(
+                i->{
+                    if(integerIntegerMap.get(i)==null){
+                        integerIntegerMap.put(i,1);
+                    }else {
+                        int val= integerIntegerMap.get(i);
+                        integerIntegerMap.put(i,val+1);
+                    }
+                }
+
+        );
         System.out.println(integerIntegerMap);
     }
 }
